dba066311b4e068554fd7125049d6847777c1825
thomas-yangy <alanyangy@sina.com>
2022-05-18 17:19:15 +0800

    add test code

---
+++ src/com/atguigu/java/eclipseKeys.java
@@ -0,0 +1,62 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class eclipseKeys {
+	
+	final static double PI = 3.14;
+	
+	public static void main(String[] args) {
+		
+		 double pi =  Math.PI;
+
+
+			ArrayList listtst = new ArrayList();
+			listtst.add(123);
+			listtst.add(123);
+			listtst.add(123);
+			listtst.add(123);
+		 
+		 
+		 listtst.add(123);
+		 listtst.add(1234);
+		 
+		 
+		 
+		    
+		    
+		    
+		 HashMap map = null; //
+		 
+
+		
+		
+		
+	}
+
+}
+
+
+class User{
+	private int age; 
+	private String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "User [age=" + age + ", name=" + name + "]";
+	}
+	
+	
+}
